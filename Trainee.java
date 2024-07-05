5e1f4a794fafb455fc99fbfb4731b11afa42f84a
ABIANBALAGAN <162068441+ABIANBALAGAN@users.noreply.github.com>
2024-07-05 15:04:54 +0530

    Add files via upload

---
+++ Trainee.java
@@ -0,0 +1,45 @@
+package com.model;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Trainee101")
+@AttributeOverrides({
+	@AttributeOverride(name="id",column=@Column(name="id")),
+	@AttributeOverride(name="name",column=@Column(name="name")),
+})
+
+public class Trainee extends Employee{
+
+	private float payperhour;
+	private String contractperiod;
+	
+	
+	public Trainee() {
+		super();
+	}
+	public Trainee(float payperhour, String contractperiod,String name) {
+		super(name);
+		this.payperhour = payperhour;
+		this.contractperiod = contractperiod;
+	}
+	public float getPayperhour() {
+		return payperhour;
+	}
+	public void setPayperhour(float payperhour) {
+		this.payperhour = payperhour;
+	}
+	public String getContractperiod() {
+		return contractperiod;
+	}
+	public void setContractperiod(String contractperiod) {
+		this.contractperiod = contractperiod;
+	}
+	
+	
+}
